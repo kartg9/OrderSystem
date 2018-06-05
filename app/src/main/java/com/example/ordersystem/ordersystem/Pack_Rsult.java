24c322fa524df773f470ab41770b5972a15e3a31
kartg <kartg9@gmail.com>
2018-06-06 00:08:01 +0800

    2018-06-06

--- app/src/main/java/com/example/ordersystem/ordersystem/Pack_Rsult.java
+++ app/src/main/java/com/example/ordersystem/ordersystem/Pack_Rsult.java
@@ -3,6 +3,8 @@ package com.example.ordersystem.ordersystem;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Pack_Rsult extends Pack_Soup {
@@ -12,19 +14,26 @@ public class Pack_Rsult extends Pack_Soup {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pack__rsult);
         Intent intent = getIntent();
-        final String Pack_Meal_Value = intent.getStringExtra("Pack_Meal_Value");
-        final String Pack_Drink_Value = intent.getStringExtra("Pack_Drink_Value");
-        final String Pack_Soup_Value = intent.getStringExtra("Pack_Soup_Value");
-        final TextView Pack_Result_Meal;
-        final TextView Pack_Result_Drink;
-        final TextView Pack_Result_Soup;
+        String Pack_Meal_Value = intent.getStringExtra("Pack_Meal_Value");
+        String Pack_Drink_Value = intent.getStringExtra("Pack_Drink_Value");
+        String Pack_Soup_Value = intent.getStringExtra("Pack_Soup_Value");
+        TextView Pack_Result_Meal;
+        TextView Pack_Result_Drink;
+        TextView Pack_Result_Soup;
         Pack_Result_Meal = (TextView) findViewById(R.id.Pack_Result_Meal);
         Pack_Result_Meal.setText(Pack_Meal_Value);
         Pack_Result_Drink = (TextView) findViewById(R.id.Pack_Result_Drink);
         Pack_Result_Drink.setText(Pack_Drink_Value);
         Pack_Result_Soup = (TextView) findViewById(R.id.Pack_Result_Soup);
         Pack_Result_Soup.setText(Pack_Soup_Value);
+        Button Pack_Result_Back = (Button)findViewById(R.id.Pack_Result_Back);
+        Pack_Result_Back.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 }
